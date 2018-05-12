5ada988b81bf9ee21e665da9a9a675dae96ceb2c
Sylvain Cadilhac <sylvain.cadilhac@jaguar-network.com>
2018-05-12 21:06:00 +0200

    Ugly patch to make this work with RNN

--- android/src/main/java/io/tradle/react/LocalAuthModule.java
+++ android/src/main/java/io/tradle/react/LocalAuthModule.java
@@ -13,13 +13,14 @@ import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ActivityEventListener;
 import com.facebook.react.bridge.BaseActivityEventListener;
 import com.facebook.react.bridge.ReadableMap;
+import com.reactnativenavigation.NavigationApplication;
 
 // source for main part from:
 // https://github.com/googlesamples/android-ConfirmCredential/blob/master/Application/src/main/java/com/example/android/confirmcredential/MainActivity.java
 
 public class LocalAuthModule extends ReactContextBaseJavaModule {
 
-  private static final int AUTH_REQUEST = 56238;
+  private static final int AUTH_REQUEST = 16238;
   private static final String E_ACTIVITY_DOES_NOT_EXIST = "E_ACTIVITY_DOES_NOT_EXIST";
   private static final String E_AUTH_CANCELLED = "LAErrorUserCancel";
   private static final String E_FAILED_TO_SHOW_AUTH = "E_FAILED_TO_SHOW_AUTH";
@@ -29,26 +30,25 @@ public class LocalAuthModule extends ReactContextBaseJavaModule {
   private KeyguardManager mKeyguardManager;
   private Promise authPromise;
 
-  private final ActivityEventListener mActivityEventListener = new BaseActivityEventListener() {
-    @Override
-    public void onActivityResult(Activity activity, int requestCode, int resultCode, Intent data) {
-      if (requestCode != AUTH_REQUEST || authPromise == null) return;
-
-      if (resultCode == Activity.RESULT_CANCELED) {
-        authPromise.reject(E_AUTH_CANCELLED, "User canceled");
-      } else if (resultCode == Activity.RESULT_OK) {
-        authPromise.resolve(true);
-      }
-
-      authPromise = null;
-    }
-  };
+  public static LocalAuthModule instance;
 
   public LocalAuthModule(ReactApplicationContext reactContext) {
     super(reactContext);
     this.reactContext = reactContext;
-    reactContext.addActivityEventListener(mActivityEventListener);
     mKeyguardManager = (KeyguardManager) this.reactContext.getSystemService(Context.KEYGUARD_SERVICE);
+    instance = this;
+  }
+
+  public void onActivityResult(int requestCode, int resultCode, Intent data) {
+    if (requestCode != AUTH_REQUEST || authPromise == null) return;
+
+    if (resultCode == Activity.RESULT_CANCELED) {
+      authPromise.reject(E_AUTH_CANCELLED, "User canceled");
+    } else if (resultCode == Activity.RESULT_OK) {
+      authPromise.resolve(true);
+    }
+
+    authPromise = null;
   }
 
   @Override
@@ -80,11 +80,12 @@ public class LocalAuthModule extends ReactContextBaseJavaModule {
     // Store the promise to resolve/reject when picker returns data
     authPromise = promise;
 
+
     String reason = map.hasKey("reason") ? map.getString("reason") : null;
     String description = map.hasKey("description") ? map.getString("description") : null;
     try {
       final Intent authIntent = mKeyguardManager.createConfirmDeviceCredentialIntent(reason, description);
-      currentActivity.startActivityForResult(authIntent, AUTH_REQUEST);
+      currentActivity.startActivityForResult(authIntent, AUTH_REQUEST, null);
     } catch (Exception e) {
       authPromise.reject(E_FAILED_TO_SHOW_AUTH, e);
       authPromise = null;
